efe7ec84e36019c7afd0d08a403b97a401ce67ac
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-06 20:13:41 +0300

    Задание 1 Шаг1 сделано

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,13 +1,41 @@
 import { Component } from 'react';
 
 export default class Feedback extends Component {
-    render() {
-        return (
-            <div>
-                <button>Good</button>
-                <button>Neutral</button>
-                <button>Bad</button>
-            </div>
-        );
-    }
+  state = {
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  };
+
+  handleGood = () => {
+    this.setState(prevState => ({ Good: prevState.Good + 1 }));
+  };
+
+  handleNeutral = () => {
+    this.setState(prevState => ({ Neutral: prevState.Neutral + 1 }));
+  };
+
+  handleBad = () => {
+    this.setState(prevState => ({ Bad: prevState.Bad + 1 }));
+  };
+
+  render() {
+    const { Good } = this.state;
+    const { Neutral } = this.state;
+    const { Bad } = this.state;
+    return (
+      <section>
+        <h2>Please leave feedback</h2>
+        <button onClick={this.handleGood}>Good</button>
+        <button onClick={this.handleNeutral}>Neutral</button>
+        <button onClick={this.handleBad}>Bad</button>
+        <ul>
+          <h3>Statistics</h3>
+          <li>Good: {Good}</li>
+          <li>Neutral: {Neutral}</li>
+          <li>Bad: {Bad}</li>
+        </ul>
+      </section>
+    );
+  }
 }
